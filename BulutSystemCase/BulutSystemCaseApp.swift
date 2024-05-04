53941a96e55bd76a181384487cc5729f4884cae1
dostmiv <mertsondas@windowslive.com>
2024-05-04 14:18:42 +0300

    Initial Commit

---
+++ BulutSystemCase/BulutSystemCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BulutSystemCaseApp.swift
+//  BulutSystemCase
+//
+//  Created by Mert Sondaş on 4.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct BulutSystemCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BulutSystemCase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BulutSystemCase
+//
+//  Created by Mert Sondaş on 4.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
